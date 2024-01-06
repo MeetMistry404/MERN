9147e4980fd8a0f3f70631b8634a888f96fc6d5d
mistrymeet <meetmistry88@gmail.com>
2024-01-06 18:31:22 +0530

    done

--- Controller/controller.js
+++ Controller/controller.js
@@ -54,4 +54,27 @@ const del = async (req, res) => {
   }
 };
 
-module.exports = { getAll, create, update, del };
+const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userExist = await User.findOne({ email });
+
+    if (!userExist) {
+      return res.status(400).json({ msg: "invalid email" });
+    }
+
+    const user = await userExist.comparePassword(password);
+
+    if (user) {
+      res.status(200).json({
+        msg: "login successful",
+        token: await userExist.generateToken(),
+        userId: res._id.toString(),
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "internal server error" });
+  }
+};
+
+module.exports = { getAll, create, update, del, signIn };
